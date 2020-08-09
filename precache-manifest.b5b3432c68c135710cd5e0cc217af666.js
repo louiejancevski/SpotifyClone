7f3a83af4bea5bd77391472ee8f660feb6ce7da5
Louie <ljanc001@fiu.edu>
2020-08-09 18:18:31 -0400

    Updates

---
+++ precache-manifest.b5b3432c68c135710cd5e0cc217af666.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c5db8791e2efda48098ccc480fa243",
+    "url": "/SpotifyClone/index.html"
+  },
+  {
+    "revision": "9d56806855b4cb652a49",
+    "url": "/SpotifyClone/static/css/main.5333a25d.chunk.css"
+  },
+  {
+    "revision": "c719ab4ed7dd7d26fd82",
+    "url": "/SpotifyClone/static/js/2.c87e87c5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SpotifyClone/static/js/2.c87e87c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d56806855b4cb652a49",
+    "url": "/SpotifyClone/static/js/main.00204265.chunk.js"
+  },
+  {
+    "revision": "b8ef8ac4ca1e886c5ad0",
+    "url": "/SpotifyClone/static/js/runtime-main.69733eb2.js"
+  },
+  {
+    "revision": "b9a2ee830454c40a44a0b7b0e0efc3a3",
+    "url": "/SpotifyClone/static/media/home.b9a2ee83.svg"
+  },
+  {
+    "revision": "0cdf3118f4706ede46ffb9c0cdd13ab0",
+    "url": "/SpotifyClone/static/media/library.0cdf3118.svg"
+  },
+  {
+    "revision": "cf21b45c896e5d27f244a0be9aeba623",
+    "url": "/SpotifyClone/static/media/logo.cf21b45c.svg"
+  },
+  {
+    "revision": "0f6375428d8f094acc24d7e67c4ebcc1",
+    "url": "/SpotifyClone/static/media/search.0f637542.svg"
+  }
+]);
