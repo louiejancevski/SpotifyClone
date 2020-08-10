9dd3ee8cb6c68e464315181a78a875c4c0598fcb
Louie <ljanc001@fiu.edu>
2020-08-10 06:28:35 -0400

    Added background color to body tag

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react"
-import "./App.css"
-import Login from "./Login"
-import { getTokenFromURL } from "../Config/spotify"
-import SpotifyWebApi from "spotify-web-api-js"
-import Player from "./Player"
-import { useDataLayerValue } from "../DataLayer"
-import { playlist } from "../variables"
+import React, { useEffect } from 'react'
+import './App.css'
+import Login from './Login'
+import { getTokenFromURL } from '../Config/spotify'
+import SpotifyWebApi from 'spotify-web-api-js'
+import Player from './Player'
+import { useDataLayerValue } from '../DataLayer'
+import { playlist } from '../variables'
 
 const spotify = new SpotifyWebApi()
 
@@ -14,46 +14,46 @@ function App() {
 
 	useEffect(() => {
 		const hash = getTokenFromURL()
-		window.location.hash = ""
+		window.location.hash = ''
 		let _token = hash.access_token
 
 		if (_token) {
 			spotify.setAccessToken(_token)
 
 			dispatch({
-				type: "SET_TOKEN",
+				type: 'SET_TOKEN',
 				token: _token,
 			})
 
 			spotify.getPlaylist(playlist).then((response) =>
 				dispatch({
-					type: "SET_DISCOVER_WEEKLY",
+					type: 'SET_DISCOVER_WEEKLY',
 					discover_weekly: response,
 				})
 			)
 
 			spotify.getMyTopArtists().then((response) =>
 				dispatch({
-					type: "SET_TOP_ARTISTS",
+					type: 'SET_TOP_ARTISTS',
 					top_artists: response,
 				})
 			)
 
 			dispatch({
-				type: "SET_SPOTIFY",
+				type: 'SET_SPOTIFY',
 				spotify: spotify,
 			})
 
 			spotify.getMe().then((user) => {
 				dispatch({
-					type: "SET_USER",
+					type: 'SET_USER',
 					user,
 				})
 			})
 
 			spotify.getUserPlaylists().then((playlists) => {
 				dispatch({
-					type: "SET_PLAYLISTS",
+					type: 'SET_PLAYLISTS',
 					playlists: playlists,
 				})
 			})
